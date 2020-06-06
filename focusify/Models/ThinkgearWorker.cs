4293ac1fcebde3968c8da403a26843f7ce77fd0f
matbut <butamateusz@gmail.com>
2020-06-06 15:56:44 +0200

    Merge

--- focusify/Models/ThinkgearWorker.cs
+++ focusify/Models/ThinkgearWorker.cs
@@ -1,6 +1,14 @@
+<<<<<<< Updated upstream
 ﻿using System;
 using System.Net.Http;
 using System.Text;
+=======
+﻿//using Accord.Fuzzy;
+using FLS;
+using FLS.Rules;
+using System;
+using System.IO;
+>>>>>>> Stashed changes
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -23,6 +31,7 @@ namespace focusify.Models
         {
             try
             {
+<<<<<<< Updated upstream
                 play("spotify:track:10PjojnHeipKeQ04nrg9dq");
                 Thread.Sleep(2000);
                 
@@ -31,6 +40,26 @@ namespace focusify.Models
                     setVolume(10 * i);
                     Thread.Sleep(2000);
                 }
+=======
+                //TODO
+                var water = new LinguisticVariable("Water");
+                var cold = water.MembershipFunctions.AddTrapezoid("Cold", 0, 0, 20, 40);
+                var warm = water.MembershipFunctions.AddTriangle("Warm", 30, 50, 70);
+                var hot = water.MembershipFunctions.AddTrapezoid("Hot", 50, 80, 100, 100);
+
+                var power = new LinguisticVariable("Power");
+                var low = power.MembershipFunctions.AddTriangle("Low", 0, 25, 50);
+                var high = power.MembershipFunctions.AddTriangle("High", 25, 50, 75);
+
+                IFuzzyEngine fuzzyEngine = new FuzzyEngineFactory().Default();
+
+                var rule1 = Rule.If(water.Is(cold).Or(water.Is(warm))).Then(power.Is(high));
+                var rule2 = Rule.If(water.Is(hot)).Then(power.Is(low));
+                fuzzyEngine.Rules.Add(rule1, rule2);
+
+                var result = fuzzyEngine.Defuzzify(new { water = 60 });
+                Console.WriteLine("This is: " + result);
+>>>>>>> Stashed changes
             }
             catch (Exception ex)
             {
